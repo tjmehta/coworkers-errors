b68ccdea95737624c8554b29f8a50a2ac1261706
Tejesh Mehta <tejesh.mehta@gmail.com>
2017-03-12 00:44:22 -0800

    fix: relax err instanceof checks

--- fatal-error.js
+++ fatal-error.js
@@ -5,6 +5,8 @@ const assert = require('assert')
 const throwNextTick = require('throw-next-tick')
 
 const AppError = require('./app-error.js')
+const isError = require('./lib/is-error.js')
+
 /**
  * FatalError - error that should kill the process
  * @class
@@ -16,7 +18,7 @@ module.exports = class FatalError extends AppError {
    * @throws {*Error} wrapped error
    */
   static throwNextTick (err, data) {
-    assert(err instanceof Error, '"err" must be an Error')
+    assert(isError(err), '"err" must be an Error')
     const newErr = FatalError.wrap(err, data)
     // adjust stack
     newErr.stack = newErr.stack.replace(/^([^\n]*\n)(.*throwNextTick[^\n]+\n)/, '$1')
